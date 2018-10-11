53a01774abad27f72fc43dde465bb5f735987e36
pagaduanjc <johnpagaduan04@gmail.com>
2018-10-11 15:07:51 +0800

    1.0.3 α

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -85,27 +85,27 @@ class ReportsController extends Controller
     }
     public function printPDF1()
     {
-        $pdf = PDF::loadView('Reports.disabledTReportPDF');
+        $pdf = PDF::loadView('Reports.disabledTReportPDF')->setPaper('letter', 'landscape');;
         return $pdf->download('disabledtugboatreport.pdf');
     }
     public function printPDF2()
     {
-        $pdf = PDF::loadView('Reports.jobOrderReportPDF');
+        $pdf = PDF::loadView('Reports.jobOrderReportPDF')->setPaper('letter', 'landscape');;
         return $pdf->download('jobOrderReport.pdf');
     }
     public function printPDF3()
     {
-        $pdf = PDF::loadView('Reports.salesReportPDF');
+        $pdf = PDF::loadView('Reports.salesReportPDF')->setPaper('letter', 'landscape');;
         return $pdf->download('salesReport.pdf');
     }
     public function printPDF4()
     {
-        $pdf = PDF::loadView('Reports.soaPDF');
+        $pdf = PDF::loadView('Reports.soaPDF')->setPaper('letter', 'landscape');
         return $pdf->download('soa.pdf');
     }
     public function printPDF5()
     {
-        $pdf = PDF::loadView('Reports.serviceReportPDF');
+        $pdf = PDF::loadView('Reports.serviceReportPDF')->setPaper('letter', 'landscape');;
         return $pdf->download('serviceReport.pdf');
     }
 }
