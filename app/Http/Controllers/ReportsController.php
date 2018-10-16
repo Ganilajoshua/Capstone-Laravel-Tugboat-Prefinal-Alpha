ba4ecea620f5bd1454af5445f986c3b8bf8d8a00
leviemarinas <leviemarina@gmail.com>
2018-10-17 00:31:08 +0800

    initial reports

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -1,10 +1,19 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use PDF;
+use Illuminate\Support\Facades\DB;
+use Auth;
+use App\Tugboat;
+use App\TugboatClass;
+use App\TugboatMainSpecifications;
+use App\TugboatSpecifications;
+use App\Company;
+use App\JobOrder;
+use App\JobSchedule;
+use App\User;
+use Redirect;
 
 class ReportsController extends Controller
 {
@@ -15,7 +24,72 @@ class ReportsController extends Controller
      */
     public function index()
     {
-        return view('Reports.index');
+        $admintbs = DB::table('tbltugboat as tugboat')
+        ->join('tbltugboatmain as main','tugboat.intTTugboatMainID','main.intTugboatMainID')
+        ->join('tblcompany as company','tugboat.intTCompanyID','company.intCompanyID')
+        ->where('tugboat.boolDeleted',0)
+        ->where('tugboat.intTCompanyID',Auth::user()->intUCompanyID)
+        ->get(); 
+
+        $services = DB::table('tbljoborder as joborder')
+        ->select(DB::raw('distinct enumServiceType,count(distinct intJobOrderID) as Service_Count, sum(fltBalanceRemain) as servicesum ') )
+        ->join('tbljobsched as jobsched', 'joborder.intJobOrderID', 'jobsched.intJSJobOrderID')
+        ->join('tbldispatchticket as dispatchticket', 'jobsched.intJobSchedID', 'dispatchticket.intDTJobSchedID')
+        ->leftJoin('tblinvoice as invoice', 'dispatchticket.intDispatchTicketID', 'invoice.intIDispatchTicketID')
+        ->where('jobsched.enumStatus','Finished')
+        ->groupBy('joborder.enumServiceType')
+        ->get();
+
+        $joborders = DB::table('tbljoborder as joborder')
+        ->join('tblcompany as company','joborder.intJOCompanyID','company.intCompanyID')
+        ->join('tbljobsched as jobsched', 'joborder.intJobOrderID', 'jobsched.intJSJobOrderID')
+        ->join('tbldispatchticket as dispatchticket', 'jobsched.intJobSchedID', 'dispatchticket.intDTJobSchedID')
+        ->join('tblinvoice as invoice', 'dispatchticket.intDispatchTicketID', 'invoice.intIDispatchTicketID')
+        ->where('jobsched.enumStatus','Finished')
+        ->groupBy('joborder.intJobOrderID')
+        ->select(DB::raw('intJobOrderID, strCompanyName, datStartDate,tmStart, sum(fltBalanceRemain) as totaljo'))
+        ->get();
+
+        // $tugboats = DB::table('tbltugboat as tugboat')
+        // ->select('strName')
+        // ->join('tbltugboatmain as tbmain', 'tugboat.intTugboatID','=', 'tbmain.intTugboatMainID')
+        // ->join('tbltugboatassign as assign', 'assign.intTATugboatID','=', 'tugboat.intTugboatID')
+        // ->join('tbljobsched as jobsched', 'jobsched.intJSTugboatAssignID','=', 'assign.intTAssignID')
+        // ->join('tbljoborder as joborder', 'joborder.intJobOrderID','=', 'jobsched.intJSJobOrderID')
+        // ->where('jobsched.enumStatus','Finished')
+        // ->get();
+
+        $sales = DB::table('tblcompany as company')
+        ->join('tbljoborder as joborder', 'joborder.intJOCompanyID', 'company.intCompanyID')
+        ->join('tbljobsched as jobsched', 'joborder.intJobOrderID', 'jobsched.intJSJobOrderID')
+        ->join('tbldispatchticket as dispatchticket', 'jobsched.intJobSchedID', 'dispatchticket.intDTJobSchedID')
+        ->leftJoin('tblinvoice as invoice', 'dispatchticket.intDispatchTicketID', 'invoice.intIDispatchTicketID')
+        ->where('jobsched.enumStatus','Finished')
+        ->where('invoice.intInvoiceID', '<>', null)
+        ->groupBy('invoice.intInvoiceID')
+        ->select(DB::raw('strCompanyName, intInvoiceID, invoice.enumStatus, sum(fltBalanceRemain) as salessum'))
+        ->get();
+
+        $consignees = DB::table('tblcompany as company')
+        ->join('tblcontractlist as contractlist', 'company.intCompanyID', 'contractlist.intCCompanyID')
+        ->leftJoin('tblbalance as balance', 'balance.intBalanceID', 'company.intCompanyID')
+        ->where('contractlist.enumStatus', 'Active')
+        ->groupBy('company.intCompanyID')
+        ->select(DB::raw('strCompanyName, datContractActive, datContractExpire, enumConValidity, enumStatus, sum(fltBalance) as fund'))
+        ->get();
+
+        // $consigneefunds = DB::table('tblcompany as company')
+        // ->leftJoin('tblbalance as balance', 'balance.intBalanceID', 'company.intCompanyID')
+        // ->get();
+
+        return view('Reports.index')
+        ->with('admintbs', $admintbs)
+        ->with('services', $services)
+        ->with('joborders', $joborders)
+        // ->with('tugboats', $tugboats)
+        ->with('sales', $sales)
+        ->with('consignees', $consignees);
+        // ->with('consigneefunds', $consigneefunds);
     }
 
     /**
@@ -83,6 +157,28 @@ class ReportsController extends Controller
     {
         //
     }
+
+    public function datepick(Request $request)
+    {
+        $id = $request->id;
+        $dates = explode('-',$request->date); // two dates MM/DD/YYYY-MM/DD/YYYY
+        $startDate = explode('/',$dates[0]); // MM[0] DD[1] YYYY[2] 
+        $finalStartDate = "$startDate[2]-$startDate[0]-$startDate[1]";
+        $endDate = explode('/',$dates[1]); // MM[0] DD[1] YYYY[2] 
+        $finalEndDate = "$endDate[2]-$endDate[0]-$endDate[1]";
+        
+        if($id == "disabledTugboats")
+    {
+        $data = DB::table('tbltugboat as tugboat')
+        ->join('tbltugboatmain as main','tugboat.intTTugboatMainID','main.intTugboatMainID')
+        ->join('tblcompany as company','tugboat.intTCompanyID','company.intCompanyID')
+        ->where('tugboat.updated_at', '>=', $finalEndDate)
+        ->get();
+        return response()->json(['data'=>$data]);
+    }
+
+    }
+
     public function printPDF1()
     {
         $pdf = PDF::loadView('Reports.disabledTReportPDF')->setPaper('letter', 'landscape');;
