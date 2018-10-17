b71a83c07d017f35136364a10d25809d292f575c
Reign <r_em11.malto@yahoo.com>
2018-10-17 10:15:50 +0800

    a

--- resources/views/DispatchTicket/info.blade.php
+++ resources/views/DispatchTicket/info.blade.php
@@ -3,8 +3,7 @@
         <div class="card-header">
             <a href="#" class="btnBack btn btn-lg btn-link float-left" data-toggle="tooltip" title="Back" role="button">
                 <i class="ion-chevron-left"></i>
-            </a>
-            <h4 class="float-right">Date: ????</h4>
+            </a>]
         </div>
         <div class="card-body">
             <div class="row">
@@ -50,7 +49,7 @@
                         <strong>Towed:</strong> <span id="towed"></span>
                     </div>
                     <div class="mb-1">
-                        <strong>Date:</strong> ????
+                        <strong>Date:</strong> <span id="date"></span>
                     </div>
                     <div class="mb-1">
                         <strong>Account:</strong> <span id="ID"></span>
@@ -76,7 +75,7 @@
                                 <td id="tugboat"></td>
                                 <td id="start"></td>
                                 <td id="destination"></td>
-                                <td>??</td>
+                                <td id="arrive"></td>
                                 <td id="service"></td>
                             </tr>
                         </tbody>
