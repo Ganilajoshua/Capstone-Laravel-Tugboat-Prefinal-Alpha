5db4469c932e080c0292b31698a152aec406caee
Reign <r_em11.malto@yahoo.com>
2018-10-28 08:30:29 +0800

    Active Contracs

--- resources/views/Contracts/index.blade.php
+++ resources/views/Contracts/index.blade.php
@@ -20,68 +20,52 @@
     </h1>
     <div class="detLayout">
         <div class="card card-primary">
-            <div class="card-header">
-                <ul class="nav nav-pills nav-fill">
-                    <li class="nav-item">
-                        <a class="nav-link showPending active" data-toggle="pill" href="#contractActivationRequestTab" role="tab" aria-controls="pills-home" aria-selected="true">Contract Activation Requests</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link showRChanges" data-toggle="pill" href="#activeContractTab" role="tab" aria-selected="false">Active Contracts</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link showAccepted" data-toggle="pill" href="#renewContractTab" role="tab">Contract Renewals</a>
-                    </li>
-                </ul>
-            </div>
             <div class="card-body">
-                <div class="tab-content" id="pills-tabContent">
-                    <div class="tab-pane fade show active" id="contractActivationRequestTab" role="tabpanel" aria-labelledby="contractActivationRequestTab-tab">
+                <div class="animated fadeIn fast">
+                    <div class="table-responsive">
+                        @if(count($company)>0)    
+                            <table class="detailedTable table table-striped text-center  mainTable" style="width:100%">
+                                <thead class="bg-primary">
+                                    <tr>
+                                        <th>Company Name</th>
+                                        <th>Contract Expiry</th>
+                                        <th>Status</th>
+                                        <th class="noSortAction">Action</th>
+                                    </tr>
+                                </thead>  
+                                <tbody>
+                                    @foreach($company as $company)
+                                        <tr>
+                                            <td>{{$company->strCompanyName}}</td>
+                                            <td>{{$company->datContractExpire}}</td>
+                                            <td>
+                                                <span class="badge badge-success"> Active</span>
+                                            </td>
+                                            <td>
+                                                <div class="ml-1 mr-1">
+                                                    <button class="btn btn-sm btn btn-primary waves-effect waves-circle" data-toggle="tooltip" title="View Details">
+                                                        <i class="fas fa-eye"></i>
+                                                    </button>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        @else
+                            <table class="detailedTable table table-striped text-center  mainTable" style="width:100%">
+                                <thead class="bg-primary">
+                                    <tr>
+                                        <th>Company Name</th>
+                                        <th class="noSortAction">Action</th>
+                                    </tr>
+                                </thead>  
+                                <tbody>
+                                </tbody>
+                            </table>
+                        @endif
                     </div>
-                    <div class="tab-pane fade" id="activeContractTab" role="tabpanel" aria-labelledby="activeContractTab-tab">
-                        <div class="animated fadeIn fast">
-                            <div class="table-responsive">
-                                @if(count($company)>0)    
-                                    <table class="detailedTable table table-striped text-center  mainTable" style="width:100%">
-                                        <thead class="bg-primary">
-                                            <tr>
-                                                <th>Company Name</th>
-                                                <th>Contract Expiry</th>
-                                                <th class="noSortAction">Action</th>
-                                            </tr>
-                                        </thead>  
-                                        <tbody>
-                                            @foreach($company as $company)
-                                                <tr>
-                                                    <td>{{$company->strCompanyName}}</td>
-                                                    <td>{{$company->datContractExpire}}</td>
-                                                    <td>
-                                                        <div class="ml-1 mr-1">
-                                                            <button class="btn btn-sm btn btn-primary waves-effect waves-circle" data-toggle="tooltip" title="View Details">
-                                                                <i class="fas fa-eye"></i>
-                                                            </button>
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
-                                @else
-                                    <table class="detailedTable table table-striped text-center  mainTable" style="width:100%">
-                                        <thead class="bg-primary">
-                                            <tr>
-                                                <th>Company Name</th>
-                                                <th class="noSortAction">Action</th>
-                                            </tr>
-                                        </thead>  
-                                        <tbody>
-                                        </tbody>
-                                    </table>
-                                @endif
-                            </div>
-                        </div>
-                    </div>
-                    <div class="tab-pane fade" id="renewContractTab" role="tabpanel" aria-labelledby="renewContractTab-tab">xd</div>
-                </div>
+                </div>        
             </div>
         </div>
     </div>
