3a448ece05fc541a405056d4117570e567d7ff8a
Reign <r_em11.malto@yahoo.com>
2018-10-29 13:12:42 +0800

    Show Teams Available

--- public/others/stisla_admin_v1.0.0/dist/js/teamassignment/teamassignmentAffiliates.js
+++ public/others/stisla_admin_v1.0.0/dist/js/teamassignment/teamassignmentAffiliates.js
@@ -54,7 +54,6 @@ $(document).ready(function(){
         }
     });
 });
-
 $('.backButton').on('click',function(){
     $('.teamAssignment').css('display','block');
     $('.teamAssignment').addClass('animated fadeIn');
@@ -63,12 +62,10 @@ $('.backButton').on('click',function(){
 
 $('.assignTeam').on('click',function(){
     console.log($(this).data('id'));
-    console.log($(this).data('id'));
     console.log('date', $(this).data('date'));
     var compDate = $(this).data('date');
     var joborderID = $(this).data('id');
-
-        $.ajax({
+    $.ajax({
         url : `${url}/getjoborderteams`,
         type : 'POST',
         data : {
@@ -86,24 +83,91 @@ $('.assignTeam').on('click',function(){
             appendTugboatTeamDefaults(data.jobsched);
             appendJoborderHeader(data.joborder);
             appendJoborderBody(data.joborder,location);
+
+            console.log(teamsavail);
+            if(teamsavail[0].length == 0){
+                var appendUnavailableTugboats =
+                `<h6>
+                    <div class=" text-danger text-center">
+                        <i class="fas fa-times mr-2 mt-2"></i> No Unavailable Teams <i class="fas fa-times ml-2"></i>
+                    </div>
+                </h6>`;
+                $(appendUnavailableTugboats).appendTo('.unavailableTugs');
+
+            }else{
+                for(var counter = 0; counter < (teamsavail[0].length); counter++){
+                    var appendUnavailableTugboats = 
+                    `<div class="card mb-2 border border-danger">
+                        <div style="margin-top: 13px; margin-bottom: 13px;"> 
+                            <div class="ml-4 mt-2 mb-2">
+                                <span class="customFontsHP">${teamsavail[0][counter].strTeamName}</span>
+                            </div>
+                        </div>
+                    </div>`;
+                    $(appendUnavailableTugboats).appendTo('.unavailableTugs');
+                }
+            }
+            if(teamsavail[1].length == 0){
+                var appendUnavailableTugboats =
+                `<h6>
+                    <div class=" text-danger text-center">
+                        <i class="fas fa-times mr-2 mt-2"></i> No Available Teams <i class="fas fa-times ml-2"></i>
+                    </div>
+                </h6>`;
+                $(appendUnavailableTugboats).appendTo('.availableTugs');
+
+            }
+            for(var counter = 0; counter < (teamsavail[1].length); counter++){
+
+                // var appendAvailableTugboats = 
+                //     `<div class="col-lg-12 col-sm-12 col-md-12">
+                //         <div class="card bg-success">
+                //             <div class="card-body">
+                //                 <div class="custom-control custom-checkbox custom-control-inline">
+                //                     <input type="checkbox" id="availableTugboat${data.tugboats[counter].intTugboatID}" data-id="${data.tugboats[counter].intTugboatID}" name="tugboatlist[]" class="custom-control-input tugboatsCheckbox">
+                //                     <label class="custom-control-label" for="availableTugboat${data.tugboats[counter].intTugboatID}">
+                //                         <p class="card-text text-center ml-2">${data.tugboats[counter].strName}<small>${data.tugboats[counter].strHorsePower}HP</small></p>
+                //                     </label>
+                //                 </div>
+                //             </div>
+                //         </div>
+                //     </div>`;
+
+                var appendAvailableTugboats = 
+                    `<div class="card mb-2 border border-success">
+                        <div style="margin-top: 13px; margin-bottom: 13px;"> 
+                            <div class="ml-4 mt-2 mb-2">
+                                <span class="customFontsHP">${teamsavail[1][counter].strTeamName}</span>
+                            </div>
+                        </div>
+                    </div>`;
+                
+                $(appendAvailableTugboats).appendTo('.availableTugs');    
+            }
             appendJoborderTeams(data.jobsched, teamsavail);
             $('.assignDefaultTeams').data('id',`${data.jobsched[0].intJSJobOrderID}`);
             $('.assignTeams').data('id',`${data.jobsched[0].intJSJobOrderID}`);
-
+            $('.assignTeams').data('date',$(this).data('date'));
             $('.teamAssignment').css('display','none');
             $('.assignTeamCard').css('display','block');
+            // $('.assignTeamCard').addClass('animated fadeIn');
         },
         error : (error)=>{
             throw error;
         }
     });
     // $.ajax({
-    //     url : `${url}/${joborderID}/getjoborder`,
-    //     type : 'GET',
-    //     dataType : 'JSON',
+    //     url : `${url}/getjoborders`,
+    //     type : 'POST',
+    //     data : {
+    //         "_token" : $('meta[name="csrf-token"]').attr('content'),
+    //         joborderID : joborderID,
+    //         compDate : compDate,
+    //     },
     //     success : (data,response)=>{
     //         console.log(data);
     //         var location = getLocation(data.joborder);
+    //         var teamsavail = getAvailableTeams(data.jobschedules, data.teams, data.joborder);
     //         appendTugboatTeamDefaults(data.jobsched);
     //         appendJoborderHeader(data.joborder);
     //         appendJoborderBody(data.joborder,location);
@@ -434,12 +498,126 @@ function submitTeamName(){
     });
 
 }
-function submitTeam(){
+
+$('.submitCreatedTeam').on('click',function(event){
+    event.preventDefault();
+    console.log('clicked');
+    
+    console.log('hi');
+    var title = $('#addTeamName').val(); 
+    var selectedmembers = [];
+
+    $('.employeesCheckbox:checkbox:checked').each(function(checked){
+        selectedmembers[checked] = {positionName : $(this).data('position'), employeeID : parseInt($(this).data('id'))};
+    });
+
+    $.ajax({
+        url : `${url}/getteamcompositions`,
+        type : 'POST',
+        data : {
+            "_token" : $('meta[name="csrf-token"]').attr('content'),    
+        },
+        success : (data)=>{
+            console.log(data);
+            console.log(selectedmembers);
+            var validationrule = [];
+            for(let count = 0; count < (data.positions).length; count++){
+                validationrule[count] = {positionName : data.positions[count].strPositionName, compositionNumber : 0};
+            }
+
+            for(let count = 0; count < selectedmembers.length; count++){
+                for(let counter = 0; counter < validationrule.length; counter++){
+                    if(selectedmembers[count].positionName === validationrule[counter].positionName){
+                        validationrule.splice(counter,1,{positionName : validationrule[counter].positionName, compositionNumber : (validationrule[counter].compositionNumber + 1)});
+                        console.log(validationrule[counter]);
+                    }
+                }
+            }
+            console.log(validationrule);
+            var errorcounter = 0;
+            for(let count=0; count < (data.positions.length); count++){
+                for(let counter=0; counter < (validationrule).length; counter++){
+                    if(data.positions[count].strPositionName === validationrule[counter].positionName){
+                        if(data.positions[count].intPositionCompNum !== validationrule[counter].compositionNumber){
+                            errorcounter =  errorcounter + 1;
+                        }
+                    }
+                }
+            }
+            membersID = [];
+            for(let count=0; count < selectedmembers.length; count++){
+                membersID[count] = parseInt(selectedmembers[count].employeeID);
+            }
+            console.log(parseInt(membersID));
+            
+            if(errorcounter > 0){
+                console.log('dami mong alam');
+                event.stopPropagation();
+                toastr.error("You did not meet the requirements needed for a team",'Team Creation Failed', { 
+                    positionClass : 'toast-bottom-right', 
+                    preventDuplicates : true, 
+                    showDuration : "2000",
+                    "showMethod": "fadeIn",
+                    "hideMethod": "fadeOut"
+                });
+            }else{
+                console.log(errorcounter);
+                console.log('eeey');
+                console.log('nagpropagate');
+                                
+                $.ajax({
+                    url : `${url}/store`,
+                    type : 'POST',
+                    data : { 
+                        "_token" : $('meta[name="csrf-token"]').attr('content'),    
+                        teamName : title,
+                        membersID : membersID,
+                    }, 
+                    beforeSend:(request)=>{
+                        return request.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));
+                    },
+                    success : (data, response)=>{
+                        console.log('success pota');
+                        console.log(data);
+                        console.log(response);
+                        swal({
+                            title: "Success",
+                            text: "Employees Assigned",
+                            type: "success",
+                            showCancelButton: false,
+                            confirmButtonClass: "btn-success",
+                            confirmButtonText: "Ok",
+                            closeOnConfirm: true,
+                            timer : 1500
+                        },
+                        (isConfirm)=>{
+                            if(isConfirm){
+                                window.location = url;
+                            }
+                        });                       
+                    },
+                    error : function(error){
+                        throw error;
+                    }
+            
+                });
+            }
+        },
+        error : (error)=>{
+
+        }
+    });
+})
+    
+function submitTeam(event){
+    event.preventDefault();
     console.log('hi');
     var title = $('#addTeamName').val(); 
     var captains = [];
     var chiefengineer = []; 
     var crew = [];
+    var selectedmembers = [];
+    var validationrule = [];
     $('.captCheckbox:checkbox:checked').each(function(checked){
         captains[checked] = parseInt($(this).val());    
     });
@@ -450,6 +628,93 @@ function submitTeam(){
         crew[checked] = $(this).val();
     });
 
+    $('.employeesCheckbox:checkbox:checked').each(function(checked){
+        selectedmembers[checked] = {positionName : $(this).data('position'), employeeID : $(this).data('id')};
+    });
+    
+    $.ajax({
+        url : `${url}/getteamcompositions`,
+        type : 'POST',
+        data : {
+            "_token" : $('meta[name="csrf-token"]').attr('content'),    
+        },
+        success : (data)=>{
+            console.log(data);
+            console.log(selectedmembers);
+            var validationrule = [];
+            for(let count = 0; count < (data.positions).length; count++){
+                validationrule[count] = {positionName : data.positions[count].strPositionName, compositionNumber : 0};
+            }
+
+            for(let count = 0; count < selectedmembers.length; count++){
+                for(let counter = 0; counter < validationrule.length; counter++){
+                    if(selectedmembers[count].positionName === validationrule[counter].positionName){
+                        validationrule.splice(counter,1,{positionName : validationrule[counter].positionName, compositionNumber : (validationrule[counter].compositionNumber + 1)});
+                        console.log(validationrule[counter]);
+                    }
+                }
+            }
+            console.log(validationrule);
+            var errorcounter = 0;
+            for(let count=0; count < (data.positions.length); count++){
+                for(let counter=0; counter < (validationrule).length; counter++){
+                    if(data.positions[count].strPositionName === validationrule[counter].positionName){
+                        if(data.positions[count].intPositionCompNum !== validationrule[counter].compositionNumber){
+                            errorcounter =  errorcounter + 1;
+                        }
+                    }
+                }
+            }
+            if(errorcounter > 0){
+                console.log('dami mong alam');
+                event.stopPropagation();
+
+            }else{
+                console.log(errorcounter);
+
+                $.ajax({
+                    url : url + '/store',
+                    type : 'POST',
+                    data : { 
+                        "_token" : $('meta[name="csrf-token"]').attr('content'),    
+                        teamName : title,
+                        
+                    }, 
+                    beforeSend:(request)=>{
+                        return request.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));
+                    },
+                    success : (data, response)=>{
+                        console.log('success pota');
+                        console.log(data);
+                        console.log(response);
+                        swal({
+                            title: "Success",
+                            text: "Employees Assigned",
+                            type: "success",
+                            showCancelButton: false,
+                            confirmButtonClass: "btn-success",
+                            confirmButtonText: "Ok",
+                            closeOnConfirm: true,
+                            timer : 1500
+                        },
+                        (isConfirm)=>{
+                            if(isConfirm){
+                                window.location = url;
+                            }
+                        });                       
+                    },
+                    error : function(error){
+                        throw error;
+                    }
+            
+                });
+            }
+        },
+        error : (error)=>{
+
+        }
+    })
+    // return false;
     if(captains.length == 0 && chiefengineer.length == 0 && crew.length == 0)
     {
         swal({
